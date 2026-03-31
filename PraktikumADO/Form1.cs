49f683c7efdc5afa992cefbae1f13b5a4e296389
sazzad <sazzad@SAZZAD_LAPTOP>
2026-03-31 20:23:49 +0700

    Add Sql library

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
