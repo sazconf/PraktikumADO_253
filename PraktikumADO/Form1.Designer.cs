54cb6e75b31ddf8518abbb68c54dc9f8e88498dc
sazzad <sazzad@SAZZAD_LAPTOP>
2026-03-31 20:32:28 +0700

    Button update code

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.btnUpdate.TabIndex = 3;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // label1
             // 
